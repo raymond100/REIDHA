b260d658f967c8eb14c7666b75c139ffa450cd85
Ray100 <jraymonddoirin@gmail.com>
2018-04-17 02:40:30 -0400

    Login Design

---
+++ app/src/main/java/com/ray100/reidha/LoginActivity.java
@@ -0,0 +1,18 @@
+package com.ray100.reidha;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ray100 on 17/04/18.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
