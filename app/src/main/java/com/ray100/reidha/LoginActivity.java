f6915c77cd36339ec6742a05594f835ec069a602
Ray100 <jraymonddoirin@gmail.com>
2018-04-17 03:48:56 -0400

    Sign up design

--- app/src/main/java/com/ray100/reidha/LoginActivity.java
+++ app/src/main/java/com/ray100/reidha/LoginActivity.java
@@ -1,8 +1,11 @@
 package com.ray100.reidha;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 /**
  * Created by ray100 on 17/04/18.
@@ -14,5 +17,17 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        final TextView register = (TextView) findViewById(R.id.link_signup);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
+
+
 }
---
+++ app/src/main/java/com/ray100/reidha/RegisterActivity.java
@@ -0,0 +1,29 @@
+package com.ray100.reidha;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import static com.ray100.reidha.R.string.register;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        final TextView login = (TextView) findViewById(R.id.link_login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
