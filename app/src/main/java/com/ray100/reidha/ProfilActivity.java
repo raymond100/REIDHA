23d1ec4c3e2499f2ce835f0ffc1afbdf56527213
Ray100 <jraymonddoirin@gmail.com>
2018-04-23 18:33:59 -0400

    Enregistrement enquete

--- app/src/main/java/com/ray100/reidha/ProfilActivity.java
+++ app/src/main/java/com/ray100/reidha/ProfilActivity.java
@@ -1,19 +1,27 @@
 package com.ray100.reidha;
 
 import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.ray100.reidha.Model.Survey;
 
 public class ProfilActivity extends AppCompatActivity {
 
     private DatabaseReference mDatabase;
     private FirebaseAuth mAuth;
+    private RecyclerView mSurveyList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +30,62 @@ public class ProfilActivity extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
 
-        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("Survey");
+
+//        mSurveyList =  findViewById(R.id.survey_list);
+//        mSurveyList.setHasFixedSize(true);
+//        mSurveyList.setLayoutManager(new LinearLayoutManager(this));
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(ProfilActivity.this, SurveyFormActivity.class));
+            }
+        });
+
+
+
+    }
+
+//    @Override
+//    protected void onStart() {
+//        super.onStart();
+//
+//        FirebaseRecyclerAdapter<Survey, SurveyViewHolder> firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<Survey, SurveyViewHolder>(
+//                Survey.class,
+//                R.layout.survey_row,
+//                SurveyViewHolder.class,
+//                mDatabase
+//        ) {
+//            @Override
+//            protected void populateViewHolder(SurveyViewHolder viewHolder, Survey model, int position) {
+//                viewHolder.setFullName(model.getNom());
+//                viewHolder.setDepartement(model.getCin());
+//            }
+//        };
+//
+//        mSurveyList.setAdapter(firebaseRecyclerAdapter);
+//    }
+
+    public static class SurveyViewHolder extends RecyclerView.ViewHolder{
+        View mview;
+        public SurveyViewHolder(View itemView) {
+            super(itemView);
+            itemView = mview;
+        }
+
+        public void setFullName(String name){
+            TextView fullName = (TextView) mview.findViewById(R.id.full_name);
+            fullName.setText(name);
+        }
+
+        public void setDepartement(String departement){
+            TextView departementText = (TextView) mview.findViewById(R.id.departement);
+            departementText.setText(departement);
+        }
+
+
     }
 
     @Override
@@ -34,11 +97,6 @@ public class ProfilActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-
-        if(item.getItemId() == R.id.action_add){
-            startActivity(new Intent(ProfilActivity.this, SurveyFormActivity.class));
-        }
-
 
         if(item.getItemId() == R.id.action_logout){
             logout();
--- app/src/main/java/com/ray100/reidha/SurveyFormActivity.java
+++ app/src/main/java/com/ray100/reidha/SurveyFormActivity.java
@@ -2,19 +2,98 @@ package com.ray100.reidha;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class SurveyFormActivity extends AppCompatActivity {
 
+    private DatabaseReference mDatabase;
+    private EditText nomEditText;
+    private EditText prenomEditText;
+    private EditText cinEditText;
+    private EditText birthEditText;
+    private Spinner sexSpinner;
+    private EditText addressEditText;
+    private Spinner situationSpinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_survey_form);
+
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("Survey");
+
+        nomEditText = (EditText) findViewById(R.id.input_first_name);
+        prenomEditText = (EditText) findViewById(R.id.input_last_name);
+        cinEditText = (EditText) findViewById(R.id.input_cin);
+        birthEditText = (EditText) findViewById(R.id.input_birth);
+        addressEditText = (EditText) findViewById(R.id.input_address);
+
+        sexSpinner =  findViewById(R.id.sex_spinner);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.sex, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        sexSpinner.setAdapter(adapter);
+
+        situationSpinner =  findViewById(R.id.situation_spinner);
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this,
+                R.array.situation, android.R.layout.simple_spinner_item);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        situationSpinner.setAdapter(adapter2);
+
+
+
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.survey_menu,menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.save_survey){
+            saveToFirebase();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void saveToFirebase(){
+        String nom = nomEditText.getText().toString().trim();
+        String prenom = prenomEditText.getText().toString().trim();
+        String cin = cinEditText.getText().toString().trim();
+        String addresse = addressEditText.getText().toString().trim();
+        String sexe = sexSpinner.getSelectedItem().toString();
+        String situation = situationSpinner.getSelectedItem().toString();
+
+        if(!TextUtils.isEmpty(nom) &&
+            !TextUtils.isEmpty(prenom) &&
+               !TextUtils.isEmpty(cin) &&
+                !TextUtils.isEmpty(addresse) &&
+                !TextUtils.isEmpty(sexe) &&
+                !TextUtils.isEmpty(situation)){
+
+            DatabaseReference survey = mDatabase.push();
+
+            survey.child("Nom").setValue(nom);
+            survey.child("Prenom").setValue(prenom);
+            survey.child("Cin").setValue(cin);
+            survey.child("Addresse").setValue(addresse);
+            survey.child("Sexe").setValue(sexe);
+            survey.child("Situation Familiale").setValue(situation);
+        }
+
+
     }
 }
